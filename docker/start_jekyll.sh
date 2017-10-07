90164ac70760919ec7eda5a92e0ea526ebd234b9
chhch <chhch+github@protonmail.com>
2017-10-07 14:46:07 +0200

    Add comments

--- docker/start_jekyll.sh
+++ docker/start_jekyll.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Remove Gemfile.lock to get always the newest versions of the Gems
 rm ../Gemfile.lock
+
+# Run the container and remove it when it finished
 docker run --rm \
   --mount type=bind,source=$PWD/..,target=/srv/jekyll \
-  --publish 4000:4000 \
+  --publish 127.0.0.1:4000:4000 \
   --name jekyll \
   jekyll/jekyll:latest \
   jekyll serve --drafts
