2a209db8f5e4ce3e114c3a81fb0670c55e2b613e
chhch <chhch+github@protonmail.com>
2018-04-28 18:28:18 +0200

    Allow to call docker script from different locations
    https://stackoverflow.com/a/1638397

--- docker/start_jekyll.sh
+++ docker/start_jekyll.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Get project path
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+PROJECT_PATH="$SCRIPT_PATH/.."
+
 # Remove Gemfile.lock to get always the newest versions of the Gems
-rm ../Gemfile.lock
+rm $PROJECT_PATH/Gemfile.lock
 
 # Run the container and remove it when it finished
 docker run --rm \
-  --mount type=bind,source=$PWD/..,target=/srv/jekyll \
+  --mount type=bind,source=$PROJECT_PATH,target=/srv/jekyll \
   --publish 127.0.0.1:4000:4000 \
   --name jekyll \
   jekyll/jekyll:latest \
