a3f20e30fdaa13e0b3af26359428f1fd6836f804
chhch <chhch@users.noreply.github.com>
2022-04-13 19:17:24 +0200

    Remove scripts from assets

--- assets/misc/fixes.sh
+++
@@ -1,6 +0,0 @@
-# W-LAN (Verbindungsprobleme beheben)
-sudo echo "options iwlwifi 11n_disable=1" >> /etc/modprobe.d/iwlwifi.conf
-
-# Nach Anmeldung ist für einige Minuten nur der Desktop zu sehen
-# [Bug #1596377](https://bugs.launchpad.net/elementaryos/+bug/1596377)
-sudo mv /etc/xdg/autostart/at-spi-dbus-bus.desktop /etc/xdg/autostart/at-spi-dbus-bus.disabled
